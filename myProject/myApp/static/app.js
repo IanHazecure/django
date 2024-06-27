234473a66b9100aef4bb263fe0e04eb8f81d8862
IanHazecure <136367549+IanHazecure@users.noreply.github.com>
2024-06-27 07:00:35 -0400

    trying load the images in js

--- myProject/myApp/static/app.js
+++ myProject/myApp/static/app.js
@@ -1,3 +1,5 @@
+
+
 let openShopping = document.querySelector('.shopping');
 let closeShopping = document.querySelector('.closeShopping');
 let list = document.querySelector('.list');
@@ -17,69 +19,73 @@ let products = [
     {
         id: 1,
         name: 'Calathea Orbifolia',
-        image: '1.PNG',
+        image: '1.png',
         price: 28000
     },
     {
         id: 2,
         name: 'Sanseviera',
-        image: '2.PNG',
+        image: '2.png',
         price: 15000
     },
     {
         id: 3,
         name: 'Monstera',
-        image: '3.PNG',
+        image: '3.png',
         price: 25000
     },
     {
         id: 4,
         name: 'Calathea Medallion',
-        image: '4.PNG',
+        image: '4.png',
         price: 12000
     },
     {
         id: 5,
         name: 'Pilea Peperomioides',
-        image: '5.PNG',
+        image: '5.png',
         price: 10000
     },
     {
         id: 6,
         name: 'Areca',
-        image: '6.PNG',
+        image: '6.png',
         price: 17000
     },
     {
         id: 7,
         name: 'Ficus Lyrata XL',
-        image: '7.PNG',
+        image: '7.png',
         price: 70000
     },
     {
         id: 8,
         name: 'Limonero',
-        image: '8.PNG',
+        image: '8.png',
         price: 20000
     },
     {
         id: 9,
         name: 'Alocasia California',
-        image: '9.PNG',
+        image: '9.png',
         price: 60000
     }
    
+
 ];
+
+
+
 let listCards  = [];
 function initApp(){
     products.forEach((value, key) =>{
         let newDiv = document.createElement('div');
         newDiv.classList.add('item');
         newDiv.innerHTML = `
-            <img src="image/${value.image}">
-            <div class="title">${value.name}</div>
-            <div class="price">${value.price.toLocaleString()}</div>
-            <button onclick="addToCard(${key})">Add To Card</button>`;
+    <img src="{% static 'image/${value.image}' %}">
+    <div class="title">${value.name}</div>
+    <div class="price">${value.price.toLocaleString()}</div>
+    <button onclick="addToCard(${key})">Add To Card</button>`;
         list.appendChild(newDiv);
     })
 }
@@ -102,14 +108,14 @@ function reloadCard(){
         if(value != null){
             let newDiv = document.createElement('li');
             newDiv.innerHTML = `
-                <div><img src="image/${value.image}"/></div>
-                <div>${value.name}</div>
-                <div>${value.price.toLocaleString()}</div>
-                <div>
-                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
-                    <div class="count">${value.quantity}</div>
-                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
-                </div>`;
+    <div><img src="image/${value.image}"/></div>
+    <div>${value.name}</div>
+    <div>${value.price.toLocaleString()}</div>
+    <div>
+        <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+        <div class="count">${value.quantity}</div>
+        <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+    </div>`;
                 listCard.appendChild(newDiv);
         }
     })
--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -27,8 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-STATICFILES_DIRS = ['myProject\myApp\static']
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Application definition
 
@@ -118,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
